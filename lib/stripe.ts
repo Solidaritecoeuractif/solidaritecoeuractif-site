7f9ea670e1c4e4885317ae191cc5123a20b169b7
Oussou AKODJENOU <solidaritecoeuractif@gmail.com>
2026-04-11 17:57:36 +0200

    Fix Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,14 +1,16 @@
-
 import Stripe from "stripe";
 
 let client: Stripe | null = null;
 
 export function stripe() {
   if (client) return client;
+
   const secret = process.env.STRIPE_SECRET_KEY;
   if (!secret) throw new Error("STRIPE_SECRET_KEY manquant.");
+
   client = new Stripe(secret, {
-    apiVersion: "2025-02-24.acacia"
+    apiVersion: "2025-04-30.basil"
   });
+
   return client;
 }
